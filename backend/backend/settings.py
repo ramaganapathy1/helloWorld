56ef49fa45d42f11490a59c878632bd3ac29aa05
ramaganapathy1 <ramaganapathy96@outlook.com>
2020-06-27 18:43:49 +0530

    django secret key removed from setting.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3e!_z_f)k3fv%1wi8@awg%hwtz3a*$klabm=&c^f++5n^6$(fz'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
